79bbfea13d7cfe6e671df45fcc1233dde707acc8
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-03-16 19:37:40 +0530

    Removed extra code and worked on error handling

--- WebBasedCalculator/index.js
+++ WebBasedCalculator/index.js
@@ -4,23 +4,62 @@ let buttons = document.querySelectorAll('button')
 
 Array.from(buttons).forEach((each)=>{
     each.onclick = (e)=>{
-        if(e.target.innerHTML === '='){
-            if(string === ''){
-                alert('Enter Something to Calculate')
+        try{
+            if(e.target.innerHTML === '='){
+                if(string === ''){
+                    alert('Enter Something to Calculate')
+                    
+                }
+                else{
+                    try{
+                        string = eval(string)
+                        if(string !== Infinity){
+                            console.log('correct')
+                            console.log(string)
+                            document.querySelector('input').value = string;
+                        }
+                        else{
+                            alert('Enter valid numbers or expressions to calculate')
+                            string = ""
+                            document.querySelector('input').value = string;
+                        }
+                        
+                    }
+                    catch(e){
+                        alert('Enter valid numbers or expressions to calculate')
+                        string = ""
+                        document.querySelector('input').value = string;
+                    }
+                }
+                
             }
+            else if(e.target.innerHTML === 'c'){
+                string = "";
+                document.querySelector('input').value = string
+            }
+            
             else{
-                string = eval(string)
-                console.log(string)
-                document.querySelector('input').value = string;
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string
             }
         }
-        else if(e.target.innerHTML === 'c'){
-            string = "";
-            document.querySelector('input').value = string
+        catch(error){
+            alert('Enter Valid numbers and expressions')
+            string = ""
+            document.querySelector('input').value = string;
         }
-        else{
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value = string
-        }
+
     }
 })
+
+let deleteBtn = document.getElementById('delBtn')
+deleteBtn.onclick=()=>{
+    if (string.length > 1){
+        string = string.slice(0,string.length - 1)
+        document.querySelector('input').value = string;
+    }
+    else{
+        string = ""
+        document.querySelector('input').value = string;
+    }
+}
