d4d8e6bd43f2933341bf06d0b7de11d570fa9188
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-04-01 13:01:14 +0530

    Added form elements

--- TaskManagement(Todos)/my-todos-app/src/Components/Main.js
+++ TaskManagement(Todos)/my-todos-app/src/Components/Main.js
@@ -7,6 +7,21 @@ export default function Main() {
             <img src='https://www.freeiconspng.com/uploads/tasks-icon-33.png' alt='logo' className='logo'/>
             <h1 className='heading'>Task Manager</h1>
         </div>
+         <form className='form-el'>
+            <div className='input-divs'>
+                <label htmlFor='taskEl'>Enter Task:</label>
+                <input type='text' id='taskEl' placeholder='Ex: Completing Assignments, etc.' className='task-el'/>
+            </div>
+            <div className='input-divs'>
+                <label htmlFor='priorityEl'>Set Priority:</label>
+                <select id='priorityEl' className='priority-el'>
+                    <option>High</option>
+                    <option>Medium</option>
+                    <option>Low</option>
+                </select>
+            </div>
+            <button type='submit' className='add-btn'>Add</button>
+         </form>
     </div>
   )
 }
