79fd9ab4f12b3c09fc81dad008332b676efe7131
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-04-02 18:16:16 +0530

    modifying the code based on editmode

--- TaskManagement(Todos)/my-todos-app/src/Components/Task.js
+++ TaskManagement(Todos)/my-todos-app/src/Components/Task.js
@@ -1,26 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Task(props) {
    const  {eachTask,handleDeleteTask} = props
+   const [isEditMode,setIsEditMode] = useState(false)
    const editTask=()=>{
-
    }
    const deleteTask=()=>{
     handleDeleteTask(eachTask.id)
    }
   return (
-        <div className='each-task-cont'>
-            <div className='main-task-cont'>
-                <input type='checkbox' id='checkBox' className='check-box' checked={eachTask.isChecked}/>
-                <label htmlFor='checkbox' className='each-task'>{eachTask.task}</label>
-            </div>
-            <div className='task-tools'>
-                <span className={`task-priority ${eachTask.priority.toLowerCase()}`}>{eachTask.priority}</span>
-                <div className=''>
-                    <button className='edit-save-del-btn' onClick={editTask}>edit</button>
-                    <button className='edit-save-del-btn' onClick={deleteTask}>delete</button>
+        <>
+          {isEditMode ? (
+            <div className='each-task-cont'>
+                <div className='main-task-cont'>
+                    <input type='checkbox' id='checkBox' className='check-box' checked={eachTask.isChecked}/>
+                    <label htmlFor='checkbox' className='each-task'>{eachTask.task}</label>
+                </div>
+                <div className='task-tools'>
+                    <span className={`task-priority ${eachTask.priority.toLowerCase()}`}>{eachTask.priority}</span>
+                    <div className=''>
+                        <button className='edit-save-del-btn' onClick={editTask}>edit</button>
+                        <button className='edit-save-del-btn' onClick={deleteTask}>delete</button>
+                    </div>
                 </div>
             </div>
-        </div>
+          ): (
+            <div className='each-task-cont'>
+                <div className='main-task-cont'>
+                    <input type='checkbox' id='checkBox' className='check-box' checked={eachTask.isChecked}/>
+                    <label htmlFor='checkbox' className='each-task'>{eachTask.task}</label>
+                </div>
+                <div className='task-tools'>
+                    <span className={`task-priority ${eachTask.priority.toLowerCase()}`}>{eachTask.priority}</span>
+                    <div className=''>
+                        <button className='edit-save-del-btn' onClick={editTask}>edit</button>
+                        <button className='edit-save-del-btn' onClick={deleteTask}>delete</button>
+                    </div>
+                </div>
+            </div>
+          )}
+        </>
   )
 }
